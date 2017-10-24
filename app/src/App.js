9bc6d06eddeb757d0fa842da9187a3b9ad95e38a
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-10-24 12:30:01 -0500

    completed 10/24

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Calculator from './components/calculator/Calculator';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
